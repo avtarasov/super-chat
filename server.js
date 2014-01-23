79e162ba04602455ef75f8e4fc98d158f1b7dc72
atarasov <astv33@gmail.com>
2014-01-23 17:46:10 +0400

    fixed.

--- server.js
+++ server.js
@@ -3,37 +3,21 @@ var sockjs  = require('sockjs');
 var http    = require('http');
 var redis   = require('redis');
 
+var echo = sockjs.createServer();
+echo.on('connection', function(conn) {
 
-// Redis publisher
-var publisher = redis.createClient();
+    setInterval(function() {
+        conn.write("test message");
+    }, 2000);
 
-// Sockjs server
-var sockjs_opts = {sockjs_url: "http://cdn.sockjs.org/sockjs-0.3.min.js"};
-var sockjs_chat = sockjs.createServer(sockjs_opts);
-sockjs_chat.on('connection', function(conn) {
-    var browser = redis.createClient();
-    browser.subscribe('chat_channel');
-
-    // When we see a message on chat_channel, send it to the browser
-    browser.on("message", function(channel, message){
-        conn.write(message);
-    });
-
-    // When we receive a message from browser, send it to be published
     conn.on('data', function(message) {
-        publisher.publish('chat_channel', message);
+        console.log(message);
     });
+
+    conn.on('close', function() {});
 });
 
-// Express server
-var app = express();
-var server = http.createServer(app);
+var server = http.createServer();
 
-sockjs_chat.installHandlers(server, {prefix:'/chat'});
-
-console.log(' [*] Listening on 0.0.0.0:9001' );
-server.listen(9001, '0.0.0.0');
-
-app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/index.html');
-});
+echo.installHandlers(server, {prefix:'/echo'});
+server.listen(9999, '0.0.0.0');
